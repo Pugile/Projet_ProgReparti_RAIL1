44a4f9795eb553ab9b03ea7b48d749c051aada43
Nathan Spath <nathan.spath4@univ-lorraine.fr>
2026-06-09 15:31:40 +0200

    LancerServiceCalcul correction

--- src/LancerServiceCalcule.java
+++ src/LancerServiceCalcule.java
@@ -1,3 +1,4 @@
+import java.rmi.NotBoundException;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -12,9 +13,17 @@ public class LancerServiceCalcule {
 
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind("ServiceCalcule", s);
+
+            Registry reg = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+            ServiceCentral sc = (ServiceCentral) reg.lookup("ServiceCentral");
+
+            sc.enregistrerClient(s);
             System.out.println("Service de calcul prêt à recevoir des tâches de calcul !");
         }
         catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        catch (NotBoundException e) {
             e.printStackTrace();
         }
     }
